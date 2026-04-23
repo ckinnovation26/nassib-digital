6e85bbcd88717a989d4f43443e721ea0a26aab63
ckinnovation26 <ckinnov@gmail.com>
2026-04-23 21:58:33 +0300

    Update sw.js

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,17 +1,1 @@
- {
-    "name": "NASSIB Restaurant",
-    "short_name": "Nassib",
-    "description": "Système de gestion restaurant By CK Innovation",
-    "start_url": "/",
-    "display": "standalone",
-    "background_color": "#0a0a1a",
-    "theme_color": "#0a0a1a",
-    "orientation": "any",
-    "icons": [
-      {
-        "src": "/favicon.ico",
-        "sizes": "64x64",
-        "type": "image/x-icon"
-      }
-    ]
-  }
+ self.addEventListener('fetch', function(event) {});
